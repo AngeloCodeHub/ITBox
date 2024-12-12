fc3997890c213d1c33cc8f7845d35fdcc00dea67
Angelo <a0920217700@gmail.com>
2024-12-13 00:24:32 +0800

    new env.json

--- Start-RDP.ps1
+++ Start-RDP.ps1
@@ -1,7 +1,7 @@
 $envObj = (Get-Content -Path ".\env.json") | ConvertFrom-Json
 $deviceObj = (Get-Content -Path ".\Confidential\Devices.json") | ConvertFrom-Json
-$RDPFile = $envObj[0].RDPFile + $deviceObj[1].Nantsi.DC1_RDP
+$RDPFile = $envObj[0].RDPFile + $deviceObj[2].Fensheng.DC_RDP
 
-Write-Host $RDPFile
+# Write-Host $RDPFile
 
 Start-Process "mstsc.EXE" -ArgumentList $RDPFile
